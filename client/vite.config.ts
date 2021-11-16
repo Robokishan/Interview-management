75a12d3e347b4033ff4c27f7e9d31a993a411ef1
Kishan Joshi <kishan.joshi@SF-CPU-218.local>
2021-11-16 13:55:57 +0530

    add: build directory to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,8 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        outDir: 'build',
+    },
     plugins: [reactRefresh(), checker({ typescript: true })],
 });

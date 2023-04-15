9da502a047e12f1b87e09e9245cc9674d38ee9d4
Kishan Joshi <robokishan.blogspot@gmail.com>
2023-04-15 15:53:29 +0530

    fix: zero downtime deployment

--- server/config/express.ts
+++ server/config/express.ts
@@ -1,17 +1,21 @@
-
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import { Request, Response } from "express";
 
 var initApp = function () {
-    var app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({
-        extended: true
-    }));
-    app.use(cookieParser());
-    console.log("version:", process.env.NODE_ENV);
-    return app;
+  var app = express();
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cookieParser());
+  // Define a health check endpoint
+  app.get("/healthz", (_req: Request, res: Response) => {
+    res.status(200).send("OK 1.4");
+  });
+  console.log("version:", process.env.NODE_ENV);
+  return app;
 };
 export default initApp;
